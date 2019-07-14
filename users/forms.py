8401f59a4f26e599fb3699940687ac0cf844f35b
meadez <m3adez@gmail.com>
2019-07-14 06:26:50 +0300

    Users app functionality (template tags, forms, views)

---
+++ users/forms.py
@@ -0,0 +1,118 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
+from django.utils.translation import ugettext_lazy as _
+
+from users.models import User
+from utils.forms import StyledForm
+
+
+class UserAuthenticationForm(StyledForm, AuthenticationForm):
+    """
+    Форма для входа пользователя в систему (Расширение стандартной формы 'AuthenticationForm')
+    """
+    remember_me = forms.BooleanField(label=_('Запомнить меня'), required=False, widget=forms.CheckboxInput())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] += ' form-control floating-label-input'
+        self.fields['username'].widget.attrs['placeholder'] = 'Email'
+        self.fields['username'].widget.attrs['id'] = 'id_username'
+        self.fields['username'].widget.attrs['type'] = 'email'
+
+        self.fields['password'].widget.attrs['class'] += ' form-control floating-label-input'
+        self.fields['password'].widget.attrs['placeholder'] = _('Пароль')
+        self.fields['password'].widget.attrs['id'] = 'id_password'
+
+        self.fields['remember_me'].widget.attrs['class'] += ' remember-me-checkbox'
+
+
+class UserPasswordResetForm(StyledForm, PasswordResetForm):
+    """
+    Форма для сброса пароля пользователем (Расширение стандартной формы 'PasswordResetForm')
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['class'] += ' form-control floating-label-input'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['email'].widget.attrs['id'] = 'id_email'
+
+
+class UserAccountActivationRepeat(UserPasswordResetForm):
+    """
+    Форма повторной активации аккаунта пользователя
+    """
+    pass
+
+
+class UserPasswordResetConfirmForm(StyledForm, SetPasswordForm):
+    """
+    Форма для сброса пароля пользователя
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['new_password1'].widget.attrs['class'] += ' form-control floating-label-input'
+        self.fields['new_password1'].widget.attrs['placeholder'] = _('Новый пароль')
+        self.fields['new_password1'].widget.attrs['id'] = 'new_password1'
+
+        self.fields['new_password2'].widget.attrs['class'] += ' form-control floating-label-input'
+        self.fields['new_password2'].widget.attrs['placeholder'] = _('Повторите пароль')
+        self.fields['new_password2'].widget.attrs['id'] = 'new_password2'
+
+
+class UserRegistrationForm(StyledForm, forms.ModelForm):
+    """
+    Форма для регистрации пользователя
+    """
+    password_repeat = forms.CharField(
+        label=_('Повторите пароль'),
+        widget=forms.PasswordInput(attrs={
+            'id': 'id_password_repeat',
+            'class': 'form-control floating-label-input',
+        }))
+
+    class Meta:
+        model = User
+        fields = ('password', 'username', 'phone_number')
+        widgets = {
+            'username': forms.EmailInput(attrs={
+                'id': 'id_username',
+                'class': 'form-control floating-label-input',
+                'placeholder': 'Email'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'id': 'id_password',
+                'class': 'form-control floating-label-input',
+                'placeholder': _('Пароль')
+            }),
+            'phone_number': forms.TextInput(attrs={
+                'id': 'id_phone_number',
+                'class': 'form-control floating-label-input',
+                'type': 'tel',
+                'placeholder': _('Номер телефона')
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password_repeat'].widget.attrs['class'] += ' form-control floating-label-input'
+        self.fields['password_repeat'].widget.attrs['placeholder'] = _('Повторите пароль')
+        self.fields['password_repeat'].widget.attrs['id'] = 'id_password_repeat'
+
+    def clean(self):
+        """
+        Проверка повторно введенного пароля
+        """
+        super().clean()
+
+        password = self.cleaned_data.get('password')
+        password_repeat = self.cleaned_data.get('password_repeat')
+        if password and password_repeat:
+            if password != password_repeat:
+                self.add_error(None, _('Пароли не совпадают'))
+
+        # Валидация пароля
+        password = self.cleaned_data.get('password')
+        password_validation.validate_password(password)
+
+        return self.cleaned_data
--- users/views.py
+++ users/views.py
@@ -1,3 +1,104 @@
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView
+from django.db import models
 from django.shortcuts import render
+from django.utils.http import urlsafe_base64_decode
+from django.views import View
+from django.views.generic import TemplateView, FormView
 
-# Create your views here.
+from users.forms import UserAuthenticationForm, UserPasswordResetForm, UserRegistrationForm, \
+    UserPasswordResetConfirmForm, UserAccountActivationRepeat
+from users.models import User
+from utils.email import EmailManager
+
+
+class UserLoginView(LoginView):
+    """
+    Представление для входа в систему
+    """
+    form_class = UserAuthenticationForm
+    redirect_authenticated_user = True
+
+    def form_valid(self, form):
+        if 'remember_me' not in form.cleaned_data:
+            self.request.session.set_expiry(settings.SET_EXPIRY)
+        return super().form_valid(form)
+
+
+class AccountActivationDoneView(TemplateView):
+    """
+    Представление для окна успешной активации аккаунта
+    """
+    template_name = 'registration/account_activation_done.html'
+
+
+class AccountActivationConfirmView(View):
+    """
+    Представление для активации аккаунта пользователя
+    """
+    def get(self, request, uidb64=None, token=None):
+        uid = urlsafe_base64_decode(uidb64)
+        user = User.objects.filter(pk=uid).first()
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.email_confirmed = True
+            user.is_active = True
+            user.save()
+
+            return render(request, 'registration/account_activation_confirm.html')
+        else:
+            return render(request, 'registration/account_activation_failed.html')
+
+
+class UserPasswordResetView(PasswordResetView):
+    """
+    Представление для сброса пароля пользователем (ввод Email)
+    """
+    form_class = UserPasswordResetForm
+
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+    """
+    Представление для сброса пароля пользователем (ввод нового пароля)
+    """
+    form_class = UserPasswordResetConfirmForm
+
+
+class UserRegistrationView(FormView):
+    """
+    Представление для регистрации пользователя
+    """
+    template_name = 'registration/registration.html'
+    form_class = UserRegistrationForm
+    success_url = '/activate/done/'
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.email = user.username
+        user.is_active = False
+        user.set_password(form.cleaned_data.get('password'))
+        user.save()
+
+        email_manager = EmailManager()
+        email_manager.send_activation_email(user)
+
+        return super().form_valid(form)
+
+
+class SendActivationEmail(FormView):
+    """
+    Представление для отправки Email активации аккаунта
+    """
+    template_name = 'registration/account_activation_repeat.html'
+    form_class = UserAccountActivationRepeat
+    success_url = '/activate/done/'
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        user = User.objects.filter(models.Q(username=email) | models.Q(email=email)).first()
+        if user and not user.is_active:
+            email_manager = EmailManager()
+            email_manager.send_activation_email(user)
+
+        return super().form_valid(form)
