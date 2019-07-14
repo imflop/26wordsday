f68fa364f57a7b76ae04c9becc28d62da57c623b
meadez <m3adez@gmail.com>
2019-07-14 06:26:01 +0300

    Edit words template and view

--- words/views.py
+++ words/views.py
@@ -2,11 +2,14 @@ from itertools import chain
 from random import choice
 
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 
 from .models import Word
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class WordListView(ListView):
     model = Word
     template_name = 'words_list.html'
@@ -17,6 +20,7 @@ class WordListView(ListView):
         qs = super().get_queryset()
         for _ in settings.ALPHABET:
             ids = qs.filter(first_letter=_).values_list('pk', flat=True)
-            item = qs.filter(pk=choice(ids))
-            self.querysets_list.append(item)
+            if ids:
+                item = qs.filter(pk=choice(ids))
+                self.querysets_list.append(item)
         return list(chain(*self.querysets_list))
