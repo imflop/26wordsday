139b519ee9c7b0705a842843c839163cdc385102
m3adez <m3adez@gmail.com>
2019-09-22 04:49:51 +0300

    Правки шаблона вывода слов

--- words/urls.py
+++ words/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from words.views import WordListView, WordDetail
 
+app_name = 'words'
+
 urlpatterns = [
     path('', WordListView.as_view(), name='words-list'),
     path('word/<slug:slug>/<int:word_id>', WordDetail.as_view(), name='word-detail')
